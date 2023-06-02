a81495612eb3498676194b090f74bf82120e9037
Christopher Carlile <cjcarlile038@gmail.com>
2023-06-02 11:51:12 -0500

    Consolidated table-component files into the bagelTable file

--- src/bagel/scripts/bagel-mainstructure/bagel-components/bagelTable.js
+++ src/bagel/scripts/bagel-mainstructure/bagel-components/bagelTable.js
@@ -1,6 +1,66 @@
 import bagelRow1 from "./bagel-table/bagelRow1";
 import bagelRow2 from "./bagel-table/bagelRow2";
+import links from "./table-components/bagelLinks";
+import discordWidget from "./table-components/discordwidget";
 
+
+/* sidebar links and placeholder */
+function bagelRow1() {
+    return (
+        <div className="bagel-Row-1">
+            {bagelCol1Row1()}
+            {bagelCol2() }
+        </div>        
+    );
+};
+
+/* handles links */
+function bagelCol1Row1() {
+    return (
+        <>
+        <div className="bagel-Col-1">
+            { links() }
+        </div>
+        </>
+        
+    );
+};
+
+/* placeholder */
+function bagelCol2() {
+    return (
+        <div className="bagel-Col-2"> 
+        
+        </div>
+    );
+};
+
+/* discord Widget, pending more columns */
+function bagelRow2() {
+    return (
+        <>
+        <div className="bagel-Row-2">
+            { bagelCol1Row2() }
+        </div>
+        </>
+        
+    );
+};
+
+export default bagelRow2
+/* Bagel Discord Widget */
+function bagelCol1Row2() {
+    return (
+        <>
+        <div className="bagel-Col-1">
+            { discordWidget() }
+        </div>
+        </>
+        
+    );
+};
+
+/* completed table*/
 function bagelTable() {
     return (
         <div className="bagel-Table">
